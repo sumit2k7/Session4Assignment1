8fa798cd9bcfb19fc11564a8501d9a105737b0b6
sumit2k7 <sumit2k7@users.noreply.github.com>
2016-04-22 12:48:14 +0530

    Added files via upload

---
+++ Session4Assignment1.java
@@ -0,0 +1,21 @@
+
+public class Session4Assignment1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	int [] first={4,3,7,2,8,9};
+	
+	System.out.print("Given Array is :-> ");
+	for (int i=0;i<first.length;i++)
+	{
+		System.out.print(first[i]+" ");
+	}
+	System.out.println();
+	System.out.print("Reverse of Array is:-> ");
+	for (int i=first.length-1;i>=0;i--)
+	{
+		System.out.print(first[i]+" ");
+	}
+	}
+
+}
